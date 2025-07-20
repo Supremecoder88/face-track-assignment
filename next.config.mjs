b3410c9837f4dabe20c0064adebae7163c92e4dc
Abhinav S P <spabhinav919@gmail.com>
2025-07-20 15:27:38 +0530

    Fix next config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+export default {
+  reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: ['http://localhost:3000'],
+  },
+};
 
-export default nextConfig;
